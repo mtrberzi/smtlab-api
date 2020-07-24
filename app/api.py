5b4e0ec95a69f745496e474275dd38699b2f227c
Murphy Berzish <murphy.berzish@gmail.com>
2020-07-24 15:45:15 -0400

    solver API

--- app/api.py
+++ app/api.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
 from flask_restful import Api, Resource, abort
 import json
+import base64
 
 from app import app, db
-from app.models import Benchmark, Instance
+from app.models import Benchmark, Instance, Solver
 from app.storage import ObjectStorageError, FileSystemObjectStorage
 
 def get_object_storage_client():
@@ -54,14 +55,20 @@ class BenchmarkAPI(Resource):
             abort(400, description="No input data provided")
         objstor = get_object_storage_client()
         objstor.ensure_bucket_exists(app.config['OBJECT_STORAGE_BENCHMARK_BUCKET'])
+        instance_data = []
         for inst in json_data:
             if 'name' not in inst or 'body' not in inst:
                 abort(400, description="Instance must specify 'name' and 'body'")
             instance = Instance(name=inst['name'], benchmark=benchmark)
             db.session.add(instance)
-            objstor.put(app.config['OBJECT_STORAGE_BENCHMARK_BUCKET'], instance.object_key(), inst['body'].encode('utf-8'))
+            instance_data.append( (instance, inst['body'].encode('utf-8')) )
         db.session.commit()
-        return ('', 204)
+        for instance, body in instance_data:
+            objstor.put(app.config['OBJECT_STORAGE_BENCHMARK_BUCKET'], instance.object_key(), body)
+        response = []
+        for instance, body in instance_data:
+            response.append(instance.json_obj_summary())
+        return response
 
     def delete(self, id):
         Benchmark.query.filter(Benchmark.id == id).delete()
@@ -99,3 +106,45 @@ class InstanceAPI(Resource):
             abort(400, description="No input data provided")
         
 api.add_resource(InstanceAPI, '/instances/<int:id>', endpoint = 'instance')
+
+class SolverListAPI(Resource):
+    def get(self):
+        all_solvers = Solver.query.all()
+        response = []
+        for solver in all_solvers:
+            response.append(solver.json_obj_summary())
+        return response
+
+    def post(self):
+        json_data = request.get_json()
+        if not json_data:
+            abort(400, description="No input data provided")
+        if "name" not in json_data or "validation_solver" not in json_data or "base64_binary" not in json_data:
+            abort(400, description="Solver must specify 'name', 'validation_solver', and 'base64_binary'")
+        if "default_arguments" in json_data:
+            args = json_data["default_arguments"]
+        else:
+            args = ""
+        solver_bytes = base64.b64decode(json_data["base64_binary"].encode('ascii'))
+        objstor = get_object_storage_client()
+        objstor.ensure_bucket_exists(app.config['OBJECT_STORAGE_SOLVER_BUCKET'])
+        solver = Solver(name=json_data["name"], validation_solver=json_data["validation_solver"], default_arguments=args)
+        db.session.add(solver)
+        db.session.commit()
+        objstor.put(app.config['OBJECT_STORAGE_SOLVER_BUCKET'], solver.object_key(), solver_bytes)
+        return solver.json_obj_summary()
+
+api.add_resource(SolverListAPI, '/solvers', endpoint = 'solver_list')
+
+class SolverAPI(Resource):
+    def get(self, id):
+        solver = Solver.query.get(id)
+        if solver is None:
+            abort(404)
+        solv = solver.json_obj_summary()
+        objstor = get_object_storage_client()
+        solver_binary = objstor.get(app.config['OBJECT_STORAGE_SOLVER_BUCKET'], solver.object_key())
+        solv['base64_binary'] = base64.b64encode(solver_binary).decode('ascii')
+        return solv
+
+api.add_resource(SolverAPI, '/solvers/<int:id>', endpoint = 'solver')
--- app/models.py
+++ app/models.py
@@ -18,3 +18,15 @@ class Instance(db.Model):
     
     def object_key(self):
         return "instance{}.smt2".format(self.id)
+
+class Solver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    validation_solver = db.Column(db.Boolean)
+    default_arguments = db.Column(db.String(1024))
+
+    def json_obj_summary(self):
+        return {'id': self.id, 'name': self.name, 'validation_solver': self.validation_solver, 'default_arguments': self.default_arguments}
+
+    def object_key(self):
+        return "solver{}.bin".format(self.id)
