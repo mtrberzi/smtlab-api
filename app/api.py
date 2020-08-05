677f4b37a71c4ca5e82d8709a3a3d7dd24c44e93
Murphy Berzish <murphy.berzish@gmail.com>
2020-08-05 16:09:37 -0400

    add run/result api

--- app/api.py
+++ app/api.py
@@ -2,9 +2,10 @@ from flask import Flask, request
 from flask_restful import Api, Resource, abort
 import json
 import base64
+import stomp
 
 from app import app, db
-from app.models import Benchmark, Instance, Solver
+from app.models import Benchmark, Instance, Solver, Run, Result, SolverResponseEnum
 from app.storage import ObjectStorageError, FileSystemObjectStorage
 
 def get_object_storage_client():
@@ -77,6 +78,17 @@ class BenchmarkAPI(Resource):
 
 api.add_resource(BenchmarkAPI, '/benchmarks/<int:id>', endpoint = 'benchmark')
 
+class BenchmarkRunsAPI(Resource):
+    def get(self, id):
+        benchmark = Benchmark.query.get(id)
+        if benchmark is None:
+            abort(404)
+        runs = []
+        for run in benchmark.runs.all():
+            runs.append(run.json_obj_summary())
+        return runs
+api.add_resource(BenchmarkRunsAPI, '/benchmarks/<int:id>/runs', endpoint = 'benchmark_runs')
+
 class InstanceListAPI(Resource):
     def get(self, id):
         benchmark = Benchmark.query.get(id)
@@ -148,3 +160,99 @@ class SolverAPI(Resource):
         return solv
 
 api.add_resource(SolverAPI, '/solvers/<int:id>', endpoint = 'solver')
+
+class SolverRunAPI(Resource):
+    def get(self, id):
+        solver = Solver.query.get(id)
+        if solver is None:
+            abort(404)
+        runs = []
+        for run in solver.runs.all():
+            runs.append(run.json_obj_summary())
+        return runs
+api.add_resource(SolverRunAPI, '/solvers/<int:id>/runs', endpoint = 'solver_runs')
+
+class RunListAPI(Resource):
+    def get(self):
+        all_runs = Run.query.all()
+        response = []
+        for run in all_runs:
+            response.append(run.json_obj_summary())
+        return response
+
+    # start new run
+    def post(self):
+        json_data = request.get_json()
+        if not json_data:
+            abort(400, description="No input data provided")
+        if "benchmark_id" not in json_data or "solver_id" not in json_data or "performance" not in json_data:
+            abort(400, description="Run must specify 'benchmark_id', 'solver_id', and 'performance'")
+        benchmark = Benchmark.query.get(json_data["benchmark_id"])
+        if benchmark is None:
+            abort(400, description="Invalid benchmark_id")
+        solver = Solver.query.get(json_data["solver_id"])
+        if solver is None:
+            abort(400, description="Invalid solver_id")
+        if "arguments" in json_data:
+            args = json_data["arguments"]
+        else:
+            args = solver.default_arguments
+        performance = json_data["performance"]
+        run = Run(benchmark=benchmark, solver=solver, arguments=args, performance=performance)
+        db.session.add(run)
+        db.commit()
+        # send message to scheduler
+        try:
+            c = stomp.Connection(app.config['QUEUE_CONNECTION'])
+            c.connect(app.config['QUEUE_USERNAME'], app.config['QUEUE_PASSWORD'], wait=True)
+            scheduler_msg = {'action': 'schedule', 'id': run.id}
+            c.send(body=json.dumps(scheduler_msg), destination='queue/scheduler')
+            c.disconnect()
+        except Exception as e:
+            pass
+        return run.json_obj_summary()
+        
+api.add_resource(RunListAPI, '/runs', endpoint='run_list')
+
+class RunAPI(Resource):
+    # get run summary
+    def get(self, id):
+        run = Run.query.get(id)
+        if run is None:
+            abort(404)
+        return run.json_obj_summary()
+
+    # run control
+    def post(self, id):
+        json_data = request.get_json()
+        if not json_data:
+            abort(400, description="No input data provided")
+        if "action" not in json_data:
+            abort(400, description="Run control must specify 'action'")
+        action = json_data['action']
+        if action == "reschedule":
+            try:
+                c = stomp.Connection(app.config['QUEUE_CONNECTION'])
+                c.connect(app.config['QUEUE_USERNAME'], app.config['QUEUE_PASSWORD'], wait=True)
+                scheduler_msg = {'action': 'schedule', 'id': run.id}
+                c.send(body=json.dumps(scheduler_msg), destination='queue/scheduler')
+                c.disconnect()
+            except Exception as e:
+                abort(500)
+        else:
+            abort(400, description="Invalid control action")
+            
+api.add_resource(RunAPI, '/runs/<int:id>', endpoint = 'run')
+
+class RunResultListAPI(Resource):
+    def get(self, id):
+        abort(500) # TODO
+
+    def post(self, id):
+        json_data = request.get_json()
+        if not json_data:
+            abort(400, description="No input data specified")
+        # TODO
+        abort(500)
+
+api.add_resource(RunResultListAPI, '/runs/<int:id>/results', endpoint = 'run_result_list')
--- app/models.py
+++ app/models.py
@@ -1,9 +1,19 @@
+import enum
 from app import db
+
+class SolverResponseEnum(enum.Enum):\
+    no_result = 0
+    sat = 1
+    unsat = 2
+    timeout = 3
+    unknown = 4
+    error = 5
 
 class Benchmark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     instances = db.relationship('Instance', backref='benchmark', lazy='dynamic', cascade='all, delete-orphan')
+    runs = db.relationship('Run', backref='benchmark', lazy='dynamic', cascade='all, delete-orphan')
 
     def json_obj(self):
         return {'id': self.id, 'name': self.name}
@@ -12,6 +22,7 @@ class Instance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
     benchmark_id = db.Column(db.Integer, db.ForeignKey('benchmark.id'))
+    results = db.relationship('Result', backref='instance', lazy='dynamic', cascade='all, delete-orphan')
 
     def json_obj_summary(self):
         return {'id': self.id, 'name': self.name}
@@ -24,9 +35,29 @@ class Solver(db.Model):
     name = db.Column(db.String(256))
     validation_solver = db.Column(db.Boolean)
     default_arguments = db.Column(db.String(1024))
+    runs = db.relationship('Run', backref='solver', lazy='dynamic', cascade='all, delete-orphan')
 
     def json_obj_summary(self):
         return {'id': self.id, 'name': self.name, 'validation_solver': self.validation_solver, 'default_arguments': self.default_arguments}
 
     def object_key(self):
         return "solver{}.bin".format(self.id)
+
+class Run(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    benchmark_id = db.Column(db.Integer, db.ForeignKey('benchmark.id'))
+    solver_id = db.Column(db.Integer, db.ForeignKey('solver.id'))
+    arguments = db.Column(db.String(1024))
+    performance = db.Column(db.Boolean)
+    results = db.relationship('Result', backref='run', lazy='dynamic', cascade='all, delete-orphan')
+
+    def json_obj_summary(self):
+        return {'id': self.id, 'benchmark_id': self.benchmark.id, 'solver_id': self.solver.id, 'arguments': self.arguments, 'performance': self.performance}
+
+class Result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    run_id = db.Column(db.Integer, db.ForeignKey('run.id'))
+    instance_id = db.Column(db.Integer, db.ForeignKey('instance.id'))
+    result = db.Column(db.Enum(SolverResponseEnum), default=SolverResponseEnum.no_result)
+    stdout = db.Column(db.UnicodeText())
+    runtime = db.Column(db.Integer) # running time in milliseconds
